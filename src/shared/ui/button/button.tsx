63a6ef46dd14182f27690bbe195ba1c76e0a3584
DanyPlaya <danila.lusuy@gmail.com>
2023-11-29 19:42:53 +0300

    feat: created Button

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,13 @@
+import "./button.css";
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+  icon?: React.ReactNode;
+};
+export const Button = (props: ButtonProps) => {
+  const { children, ...buttonProps } = props;
+  return (
+    <button className={`button` + buttonProps.className} {...buttonProps}>
+      {children}
+    </button>
+  );
+};
