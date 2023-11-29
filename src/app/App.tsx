db33b895062745de8352a58f66da5896caa41321
DanyPlaya <danila.lusuy@gmail.com>
2023-11-29 19:46:23 +0300

    feat: add header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
 import "./globalStyles.css";
+import { Header } from "../widgets";
 const tg = Telegram.WebApp;
 export const App = () => {
   useEffect(() => {
     tg.ready();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
