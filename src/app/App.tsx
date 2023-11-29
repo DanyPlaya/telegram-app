40727f90d7af172d4bb14a2decf8955cb617a053
DanyPlaya <danila.lusuy@gmail.com>
2023-11-29 21:39:25 +0300

    feat: add useTelegram hook, and telegram button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import "./globalStyles.css";
-import { Header } from "../widgets";
-const tg = Telegram.WebApp;
+import { Header } from "@/widgets";
+import { useTelegram } from "@/shared/hooks";
+
 export const App = () => {
+  const { tg, onToggleButton } = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
-    <div>
+    <div className="layout">
       <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 };
